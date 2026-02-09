50b9a8713c4f0abfd3ceea2573abc45576f1acfa
taichi-k <no3b1219@gmail.com>
2026-02-09 22:05:14 +0900

    github pagesへ自動デプロイできるようにした

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',  // 静的HTMLとして書き出す
+  basePath: isProd ? '/life-plan-visualizer' : '',
+  assetPrefix: isProd ? '/life-plan-visualizer/' : '',
 };
 
 export default nextConfig;
